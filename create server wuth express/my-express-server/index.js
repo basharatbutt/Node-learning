bbbe85e2594fd82017cc434cedda7c4c32a688e7
basharatbutt <basharatdev57@gmail.com>
2026-04-01 15:50:45 +0500

    m

---
+++ create server wuth express/my-express-server/index.js
@@ -0,0 +1,23 @@
+// 1. Import Express
+const express = require('express');
+
+// 2. Initialize the Express application
+const app = express();
+
+// 3. Define a Port number
+const PORT = 3000;
+
+// 4. Create a "Route" (What happens when someone visits your website)
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to my Express Server!</h1>');
+});
+
+// 5. Create an "About" Route
+app.get('/about', (req, res) => {
+    res.send('<h3>This is the About Page</h3>');
+});
+
+// 6. Start the server and "Listen" for requests
+app.listen(PORT, () => {
+    console.log(`✅ Server is running at http://localhost:${PORT}`);
+});
