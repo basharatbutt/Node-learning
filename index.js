fdf98a01d37421c54276ecb60ecf529f98213f27
basharatbutt <basharatdev57@gmail.com>
2026-04-01 12:53:48 +0500

    s

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const http = require("http");
 const fs = require("fs")
 
 const myServer = http.createServer( (req, res) => {
-    const log = `${Date.now()}: New `
-      fs.appendFile('log.txt,')
-    res.end("Hello From Server");
+    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+      fs.appendFile('log.txt', log, (err, data) => {
+        switch (req.url) {
+          case '/': res.end("Hello HomePage");
+            break;
+          case'/about': res.end("I am About Page");
+            break;
+            default:
+              res.end("404 Not Found");
+        }
+      
+      });
+    
 });
 
 myServer.listen(8000, () => console.log("Server started") );
