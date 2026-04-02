a0bd0ae2799d30232d1f185cae2516e8860e472b
basharatbutt <basharatdev57@gmail.com>
2026-04-02 10:59:42 +0500

    Update index.js

--- create server/index.js
+++ create server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send("Hello kya hal hai")
+  res.send("Hello kya  hai")
 })
 
 app.listen(3000, () => {
