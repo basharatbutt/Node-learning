941e1d01fa91396b6755ce47b68f201bcda972e4
basharatbutt <basharatdev57@gmail.com>
2026-04-17 17:51:05 +0500

    g

--- Express js and node js Authentication/index.js
+++ Express js and node js Authentication/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
 
+//database connection
+mongoose.connect('mongodb://127.0.0.1:27017/user-crud')
+.then( () => console.log('Connected to database'))
+
+//midleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.set("view engine", "ejs");
+
+//routes
 app.get('/', (req, res) => {
     res.send('hello world');
+})
+
+app.get('/login', (req, res) => {
+    res.send('login page');
+})
+
+app.get('/register', (req, res) => {
+    res.send('register page');
 })
 
 app.listen(3000, () => {
